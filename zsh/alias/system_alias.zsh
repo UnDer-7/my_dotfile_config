b3d89ef281057e2100c4d0a701f32ec7cc3ae7c3
UnDer-7 <mateus7532@gmail.com>
2025-07-16 21:27:43 -0300

    added some docs

--- zsh/alias/system_alias.zsh
+++ zsh/alias/system_alias.zsh
@@ -4,4 +4,7 @@ alias sa="ls -a"
 alias R="rm -rf"
 alias cl=clear
 alias lc=clear
+
+# xclip must be installed
+# how do to use: `cat some-file.txt | copy` than you can just ctrl+v the some-file.txt content
 alias copy="xclip -selection clipboard"
